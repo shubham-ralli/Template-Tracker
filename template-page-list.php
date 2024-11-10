1eba28e2dbc33ba2be8268c3832b95586eea9613
shubham ralli <sralli73@gmail.com>
2024-11-10 13:49:57 +0530

    bug fixed

--- template-page-list.php
+++ template-page-list.php
@@ -8,7 +8,7 @@
  * Author URI:        https://github.com/shubham-ralli
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       template-tracker
+ * Text Domain:       template-tracker-main
  */
 
 defined('ABSPATH') or die('No script kiddies please!');
@@ -16,16 +16,13 @@ defined('ABSPATH') or die('No script kiddies please!');
 // Enqueue scripts and styles
 function tpl_enqueue_scripts() {
 
+    // Enqueue DataTables script and CSS from local files
+    wp_enqueue_script('tpl-script-table', plugin_dir_url(__FILE__) . 'js/dataTables.min.js', array('jquery'), '2.1.8', true);
+    wp_enqueue_style('tpl-admin-style-table', plugin_dir_url(__FILE__) . 'css/dataTables.dataTables.min.css', array(), '2.1.8');
 
-    wp_enqueue_script('tpl-script-table', 'https://cdn.datatables.net/2.1.8/js/dataTables.min.js', array('jquery'), '1', true);
-
-    wp_enqueue_script('tpl-script', plugin_dir_url(__FILE__) . 'tpl-script.js', array('jquery'), '1', true);
-    wp_localize_script('tpl-script', 'tpl_ajax', array('ajax_url' => admin_url('admin-ajax.php')));
-    
-    // Enqueue admin CSS for styling
-    wp_enqueue_style('tpl-admin-style', plugin_dir_url(__FILE__) . 'css/tpl-style.css', array(), '1');
-
-    wp_enqueue_style('tpl-admin-style-table', 'https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css', array(), '1');
+    wp_enqueue_script('tpl-script', plugin_dir_url(__FILE__) . 'js/tpl-script.js', array('jquery'), '3', true);
+    wp_localize_script('tpl-script', 'tpl_ajax', array('ajax_url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('tpl_nonce')));
+    wp_enqueue_style('tpl-admin-style', plugin_dir_url(__FILE__) . 'css/tpl-style.css', array(), '2.1');
 }
 add_action('admin_enqueue_scripts', 'tpl_enqueue_scripts');
 
@@ -35,8 +32,6 @@ function tpl_create_admin_page() {
 }
 add_action('admin_menu', 'tpl_create_admin_page');
 
-
-
 // Add "Settings" and "Deactivate" links on the Plugins page
 function sf_plugin_action_links( $links ) {
     $settings_link = '<a href="' . admin_url( 'tools.php?page=tpl-page-list' ) . '">Settings</a>';
@@ -45,88 +40,76 @@ function sf_plugin_action_links( $links ) {
 }
 add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'sf_plugin_action_links' );
 
-
 // Render the admin page
 function tpl_render_admin_page() {
     $templates = wp_get_theme()->get_page_templates();
     ?>
-<div class="template-tracker-wrap">
-    <h1>Template Tracker</h1>
-
-
-    <div class="template-tracker-from">
-
-    <div id="error" style="display:none;"></div>
-
-    <div class="ttm-frm-group">
-            <label for="template-select">Select a Template:</label>
-            <select id="template-select">
-                <option value="">Select a Template</option>
-                <option value="default">Default Template</option> <!-- Default Template Option -->
-                <?php foreach ($templates as $template_name => $template_file) : ?>
-                    <option value="<?php echo esc_html($template_name); ?>"><?php echo esc_attr($template_file); ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-
-
-        <div class="ttm-frm-group">
-            <label for="status-select">Select Post Status:</label>
-            <select id="status-select">
-                <option value="">All Statuses</option>
-                <option value="publish">Published</option>
-                <option value="draft">Draft</option>
-                <option value="private">Private</option>
-                <option value="trash">Trash</option>
-            </select>
-        </div>
-
-        <button id="search-button" class="button">Search Posts <div id="loader" style="display:none;"></div></button>
-
-    </div>
-
-    <div class="result" id="template-pages" style="display:none;">
-
-        <div class="result_header">
-            <div class="header_left">
-                <h3>Result</h3>
+    <div class="template-tracker-wrap">
+        <h1>Template Tracker</h1>
+        <div class="template-tracker-from">
+            <div id="error" style="display:none;"></div>
+            <div class="ttm-frm-group">
+                <label for="template-select">Select a Template:</label>
+                <select id="template-select">
+                    <option value="">Select a Template</option>
+                    <option value="default">Default Template</option>
+                    <?php foreach ($templates as $template_name => $template_file) : ?>
+                        <option value="<?php echo esc_html($template_name); ?>"><?php echo esc_attr($template_file); ?></option>
+                    <?php endforeach; ?>
+                </select>
             </div>
-            <div class="header_right"><button id="download-csv" class="button" style="display:none;">Download
-                    CSV</button></div>
+            <div class="ttm-frm-group">
+                <label for="status-select">Select Post Status:</label>
+                <select id="status-select">
+                    <option value="">All Statuses</option>
+                    <option value="publish">Published</option>
+                    <option value="draft">Draft</option>
+                    <option value="private">Private</option>
+                    <option value="trash">Trash</option>
+                </select>
+            </div>
+            <button id="search-button" class="button">Search Posts <div id="loader" style="display:none;"></div></button>
         </div>
-        <table class="widefat display" id="myTable">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Template Name</th>
-                    <th>Type</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
-        <p id="total-results" style="display:none;"></p>
-
-
+        <div class="result" id="template-pages" style="display:none;">
+            <div class="result_header">
+                <div class="header_left">
+                    <h3>Result</h3>
+                </div>
+                <div class="header_right"><button id="download-csv" class="button" style="display:none;">Download CSV</button></div>
+            </div>
+            <table class="widefat display" id="myTable">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Template Name</th>
+                        <th>Type</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+            <p id="total-results" style="display:none;"></p>
+        </div>
     </div>
-</div>
-<?php
+    <?php
 }
 
 // AJAX function to fetch pages
 function tpl_fetch_pages() {
-    $template = sanitize_text_field($_POST['template']);
-    $status = sanitize_text_field($_POST['status']);
+    // Check nonce for security
+    check_ajax_referer('tpl_nonce', 'nonce');
 
-    // Prepare arguments to get all post types
+    // Check if template and status are set, then sanitize and unslash
+    $template = isset($_POST['template']) ? sanitize_text_field(wp_unslash($_POST['template'])) : '';
+    $status = isset($_POST['status']) ? sanitize_text_field(wp_unslash($_POST['status'])) : '';
+
     $args = array(
-        'post_type' => 'any', // Get all post types
-        'posts_per_page' => -1, // Get all posts
+        'post_type' => 'any',
+        'posts_per_page' => -1,
     );
 
-    // If a specific template is selected, add it to the query
     if ($template) {
         $args['meta_query'] = array(
             array(
@@ -136,24 +119,23 @@ function tpl_fetch_pages() {
         );
     }
 
-    // If a specific post status is selected, add it to the query
     if ($status) {
         $args['post_status'] = $status;
     } else {
-        $args['post_status'] = array('publish', 'draft', 'private', 'trash'); // Default to all statuses
+        $args['post_status'] = array('publish', 'draft', 'private', 'trash');
     }
 
     $pages = get_posts($args);
     $output = array();
-    
+
     foreach ($pages as $index => $page) {
         $output[] = array(
-            'index' => $index + 1, // Add index for display
+            'index' => $index + 1,
             'title' => $page->post_title,
             'template' => get_post_meta($page->ID, '_wp_page_template', true),
-            'type' => $page->post_type, // Retrieve the actual post type
-            'status' => $page->post_status, // Retrieve the post status
-            'url' => get_permalink($page->ID) // Include the permalink
+            'type' => $page->post_type,
+            'status' => $page->post_status,
+            'url' => get_permalink($page->ID)
         );
     }
     wp_send_json($output);
